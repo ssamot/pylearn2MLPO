e6f531dc964684efbadf9a6a3d46d3637a389de6
ssamot <spyridon.samothrakis@gmail.com>
2015-01-16 22:29:41 +0000

    Added incremental learning

--- sknn/pylearn2mplo.py
+++ sknn/pylearn2mplo.py
@@ -19,7 +19,7 @@ class pylearn2MLPO():
     you have access all your data upfront
     """
 
-    def __init__(self, layers, dropout = False, input_normaliser = None, output_normaliser = None,   learning_rate=0.01, verbose=0):
+    def __init__(self, layers, dropout = False, input_scaler = None, output_scaler = None,   learning_rate=0.01, verbose=0):
         """
 
         :param layers: List of tuples of types of layers alongside the number of neurons
@@ -35,6 +35,29 @@ class pylearn2MLPO():
         if(dropout):
             cost = Dropout()
         self.trainer = sgd.SGD(learning_rate=learning_rate, cost = cost, batch_size=1)
+
+
+        self.input_normaliser = input_scaler
+        self.output_normaliser = output_scaler
+
+
+    def __scale(self,X,y):
+        if(self.input_normaliser is not None):
+            X_s = self.input_normaliser.fit(X).transform(X)
+        else:
+            X_s = X
+        if(self.output_normaliser is not None and y is not None):
+            y_s = self.output_normaliser.fit(y).transform(y)
+        else:
+            y_s = y
+
+        return X_s,y_s
+
+    def __original_y(self,y):
+        if(self.output_normaliser is None):
+            return y
+        else:
+            return self.output_normaliser.inverse_transform(y)
 
     def __linit(self, X, y):
         if(self.verbose > 0):
@@ -120,8 +143,10 @@ class pylearn2MLPO():
             self.__linit(X, y)
 
         ds = self.ds
-        ds.X = X
-        ds.y = y
+        X_s,y_s = self.__scale(X,y)
+        ds.X = X_s
+        ds.y = y_s
+        #print X_s,y_s
         self.trainer.train(dataset=ds)
         return self
 
@@ -131,5 +156,80 @@ class pylearn2MLPO():
         :param X:
         :return:
         """
-        return self.f(X)
+        X_s,_ = self.__scale(X, None)
+        y =  self.f(X_s)
+        y_s = self.__original_y(y)
 
+        return y_s
+
+class IncrementalMinMaxScaler():
+    def __init__(self, feature_range=(0.0,1.0)):
+        self.feature_range = feature_range
+        self.changed = False
+        self.init = False
+        self.times = 0
+
+
+    def fit(self,X, y = None):
+        #print "fitting"
+        self.changed = False
+        self.times+=1;
+        #print "fitting"
+        if (not self.init):
+            self.min_ = np.array(X[0],dtype = np.float64)
+            self.max_ = np.array(X[0],dtype = np.float64)
+            self.data_min = self.min_
+            self.data_max = self.max_
+            self.init = True
+        else:
+            #print "appednign"
+            X = np.array(X,ndmin=2)
+            X = np.append(X, [self.data_min], axis = 0)
+            X = np.append(X, [self.data_max], axis = 0)
+
+        feature_range = self.feature_range
+        data_min = np.min(X, axis=0, )
+        data_max = np.max(X, axis=0)
+
+        if((self.data_min == data_min).all() and (self.data_max==data_max).all()):
+            pass
+        else:
+            #print "changed"
+            self.changed = True
+
+
+        self.data_min = data_min
+        self.data_max = data_max
+
+        data_range = data_max - data_min
+        # Do not scale constant features
+        #print data_range
+        #exit()
+        data_range[data_range == 0.0] = 1.0
+        data_range[data_range == 0] = 1.0
+        #print "data range", data_range.dtype, data_max.dtype, data_min.dtype
+        self.scale_ = (feature_range[1] - feature_range[0]) / data_range
+        self.min_ = feature_range[0] - data_min * self.scale_
+        self.data_range = data_range
+        self.data_min = data_min
+        return self
+
+
+    def transform(self, X):
+        #X *= self.scale_#X += self.min_
+        assert (len(X.shape) == 2), X
+        transformed =  (X * self.scale_) + self.min_
+        #transformed[:, -1] = 1.0
+        return transformed
+
+
+    def inverse_transform(self, X):
+        """Undo the scaling of X according to feature_range.
+
+        Parameters
+        ----------
+        X : array-like with shape [n_samples, n_features]
+            Input data that will be transformed.
+        """
+        reverse_transformed =  (X - self.min_)/self.scale_
+        return reverse_transformed
--- test/TestMultivariateRegression.py
+++ test/TestMultivariateRegression.py
@@ -3,20 +3,53 @@ __author__ = 'ssamot'
 from sklearn import datasets
 from sklearn.preprocessing import MinMaxScaler
 
-from sknn.pylearn2mplo import pylearn2MLPO
+from sknn.pylearn2mplo import pylearn2MLPO, IncrementalMinMaxScaler
 from sklearn.metrics import r2_score
 
 import random
 
-dataset = datasets.load_linnerud()
-X = dataset["data"]
-y = dataset["target"]
+
+def test_multivariate_regression_incremental():
+    dataset = datasets.load_linnerud()
+    X = dataset["data"]
+    y = dataset["target"]
+    layers = [("RectifiedLinear", 250),("RectifiedLinear", 250), ("Linear", )]
+    #layers = [("Maxout", 250, 2), ("Maxout", 250,2), ("Linear", )]
+    #layers = [("Tanh", 250, 2), ("Tanh", 250,2), ("Linear", )]
+    #layers = [("Sigmoid", 250, 2), ("Sigmoid", 250,2), ("Linear", )]
+
+    nn = pylearn2MLPO(layers, learning_rate=0.1, input_scaler=IncrementalMinMaxScaler(), output_scaler=IncrementalMinMaxScaler(), verbose = 10)
+
+    for k in range(0, 10000):
+        tf = list(range(0, X.shape[0]))
+        random.shuffle(tf)
+        for i in tf:
+            #ds = data(X[i:i+1], targets[i:i+1])
+            X_i = X[i:i + 1]
+            y_i = y[i:i + 1]
+            nn.fit(X_i, y_i)
+            #print X_i, y_i
+            # ann.monitor.report_epoch()
+            # ann.monitor()
+
+    y_pred = nn.predict(X)
+    r_0 = r2_score(y[:,0],y_pred[:,0] )
+    r_1 = r2_score(y[:,1],y_pred[:,1] )
+    r_2 = r2_score(y[:,2],y_pred[:,2] )
+    print r_0,r_1,r_2
+    assert(r_0 > 0.99)
+    assert(r_1 > 0.99)
+    assert(r_2 > 0.99)
 
 
 def test_multivariate_regression():
-    layers = [("RectifiedLinear", 250), ("RectifiedLinear", 250), ("Linear", )]
+    dataset = datasets.load_linnerud()
+    X = dataset["data"]
+    y = dataset["target"]
+    layers = [("RectifiedLinear", 250),("RectifiedLinear", 250), ("Linear", )]
     #layers = [("Maxout", 250, 2), ("Maxout", 250,2), ("Linear", )]
     #layers = [("Tanh", 250, 2), ("Tanh", 250,2), ("Linear", )]
+    #layers = [("Sigmoid", 250, 2), ("Sigmoid", 250,2), ("Linear", )]
 
     nn = pylearn2MLPO(layers, learning_rate=0.1, verbose = 10)
 
@@ -38,10 +71,11 @@ def test_multivariate_regression():
             # ann.monitor()
 
     y_pred = nn.predict(X_new)
-    r_0 = r2_score(y_new[:,0],y_pred[:,0] )
-    r_1 = r2_score(y_new[:,1],y_pred[:,1] )
-    r_2 = r2_score(y_new[:,2],y_pred[:,2] )
-
+    y_pred = mm.inverse_transform(y_pred)
+    r_0 = r2_score(y[:,0],y_pred[:,0] )
+    r_1 = r2_score(y[:,1],y_pred[:,1] )
+    r_2 = r2_score(y[:,2],y_pred[:,2] )
+    print r_0,r_1,r_2
     #print y_new
     #print y_pred
     assert(r_0 > 0.99)
